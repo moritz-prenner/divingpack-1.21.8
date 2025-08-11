043cd2034642797e1d64d1fa550de59b12488fc6
moritz-prenner <moritz.prenner@icloud.com>
2025-08-11 11:59:03 +0200

    v2.3

--- src/main/java/com/example/DivingPack.java
+++ src/main/java/com/example/DivingPack.java
@@ -1,14 +1,22 @@
 package com.example;
 
-
 import items.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.passive.PigEntity;
+import net.minecraft.item.Items;
 
 public class DivingPack implements ModInitializer {
     public static final String MOD_ID = "divingpack";
 
     @Override
     public void onInitialize() {
+        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
+            if (entity instanceof PigEntity) {
+                entity.dropItem(world, Items.CHAIN);
+            }
+        });
         ModItems.registerModItems();
+
     }
 }
