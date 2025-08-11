0aa85fd1e57ee63afb42e468eebb3f36fae675b2
moritz-prenner <moritz.prenner@icloud.com>
2025-08-11 15:17:30 +0200

    v2.4

--- src/main/java/com/example/DivingPack.java
+++ src/main/java/com/example/DivingPack.java
@@ -3,20 +3,19 @@ package com.example;
 import items.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.minecraft.entity.passive.PigEntity;
-import net.minecraft.item.Items;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 
 public class DivingPack implements ModInitializer {
     public static final String MOD_ID = "divingpack";
 
     @Override
     public void onInitialize() {
+        ModItems.registerModItems();
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity instanceof PigEntity) {
-                entity.dropItem(world, Items.CHAIN);
+            if (entity instanceof PlayerEntity) {
+                ((PlayerEntity) entity).sendMessage(Text.literal("hiii"), true);
             }
         });
-        ModItems.registerModItems();
-
     }
 }
--- src/main/java/items/ModItems.java
+++ src/main/java/items/ModItems.java
@@ -4,6 +4,8 @@ import com.example.DivingPack;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.equipment.EquipmentAsset;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -12,6 +14,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item DIVING_WATCH = registerItem("diving_watch", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DivingPack.MOD_ID,"diving_watch")))));
+
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(DivingPack.MOD_ID, name), item);
