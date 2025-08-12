41e8a4e2bb2bf7ea4d4e66628df66450c3824128
moritz-prenner <moritz.prenner@icloud.com>
2025-08-12 11:52:02 +0200

    v2.5

--- src/main/java/com/example/DivingPack.java
+++ src/main/java/com/example/DivingPack.java
@@ -2,9 +2,16 @@ package com.example;
 
 import items.ModItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.minecraft.entity.player.PlayerEntity;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.EquipmentAssetKeys;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.item.equipment.ArmorMaterial.*;
 
 public class DivingPack implements ModInitializer {
     public static final String MOD_ID = "divingpack";
@@ -12,10 +19,19 @@ public class DivingPack implements ModInitializer {
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
-        ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
-            if (entity instanceof PlayerEntity) {
-                ((PlayerEntity) entity).sendMessage(Text.literal("hiii"), true);
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                boolean holdsItem = isHoldingItem(player);
+                if (holdsItem) {
+                    int playerBlockY = player.getBlockY();
+                    player.sendMessage(Text.literal(String.valueOf("Your current height is: " + playerBlockY)).formatted(Formatting.BLUE), true);
+                }
             }
         });
+    }
+
+    private boolean isHoldingItem(ServerPlayerEntity player) {
+        return player.getStackInHand(Hand.MAIN_HAND).isOf(ModItems.DIVING_WATCH) || player.getStackInHand(Hand.OFF_HAND).isOf(ModItems.DIVING_WATCH);
     }
 }
--- src/main/java/items/ModItems.java
+++ src/main/java/items/ModItems.java
@@ -4,8 +4,7 @@ import com.example.DivingPack;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.equipment.EquipmentAsset;
-import net.minecraft.item.equipment.EquipmentAssetKeys;
+import net.minecraft.item.equipment.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -14,16 +13,20 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item DIVING_WATCH = registerItem("diving_watch", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DivingPack.MOD_ID,"diving_watch")))));
-
+    public static final Item DIVING_FINS = registerItem("diving_fins", new Item(new Item.Settings().armor(ArmorMaterials.IRON, EquipmentType.BOOTS).registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(DivingPack.MOD_ID,"diving_fins")))));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(DivingPack.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
             entries.add(DIVING_WATCH);
+
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(DIVING_FINS);
         });
     }
 }
